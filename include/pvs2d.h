2d5d7c992419ed568a5b944a0815290d05570141
Laudanum <86309764+Laudanum-dev@users.noreply.github.com>
2022-05-05 20:35:03 +0500

    add this fucking function

--- include/pvs2d.h
+++ include/pvs2d.h
@@ -7,43 +7,44 @@
 // --------------------------------------------------------
 
 typedef struct PVS2D_Line {
-    int ax, ay, bx, by;
-    struct PVS2D_SegStack* mems;
+	int ax, ay, bx, by;
+	struct PVS2D_SegStack* mems;
 } PVS2D_Line;
 
 typedef struct PVS2D_Seg {
-    struct PVS2D_Line* line;
-    float tStart, tEnd;
-    int opq;
+	struct PVS2D_Line* line;
+	float tStart, tEnd;
+	int opq;
 } PVS2D_Seg;
 
 typedef struct PVS2D_SegStack {
-    struct PVS2D_Seg* seg;
-    struct PVS2D_SegStack* next;
+	struct PVS2D_Seg* seg;
+	struct PVS2D_SegStack* next;
 } PVS2D_SegStack;
 
 typedef struct PVS2D_BSPTreeNode {
-    struct PVS2D_BSPTreeNode* left;
-    struct PVS2D_BSPTreeNode* right;
-    unsigned int leftLeaf;
-    unsigned int rightLeaf;
-    struct PVS2D_Line* line;
-    struct PVS2D_SegStack* segs;
-    
-    float tSplitStart;
-    float tSplitEnd;
-    struct PVS2D_PortalStack* portals;
+	struct PVS2D_BSPTreeNode* left;
+	struct PVS2D_BSPTreeNode* right;
+	unsigned int leftLeaf;
+	unsigned int rightLeaf;
+	struct PVS2D_Line* line;
+	struct PVS2D_SegStack* segs;
+	
+	float tSplitStart;
+	float tSplitEnd;
+	struct PVS2D_PortalStack* portals;
 } PVS2D_BSPTreeNode;
 
 typedef struct PVS2D_PortalStack {
-    struct PVS2D_Portal* portal;
-    struct PVS2D_PortalStack* next;
+	struct PVS2D_Portal* portal;
+	struct PVS2D_PortalStack* next;
+	int left;
 } PVS2D_PortalStack;
 
 typedef struct PVS2D_Portal {
-    PVS2D_Seg seg;
-    unsigned int leftLeaf;
-    unsigned int rightLeaf;
+	PVS2D_Seg seg;
+	unsigned int leftLeaf;
+	unsigned int rightLeaf;
 } PVS2D_Portal;
 
 // --------------------------------------------------------
@@ -51,13 +52,17 @@ typedef struct PVS2D_Portal {
 // --------------------------------------------------------
 
 int PVS2D_BuildBSPTree(
-    int* segs, unsigned int segsC,
-    PVS2D_BSPTreeNode* rootDest
+	int* segs, unsigned int segsC,
+	PVS2D_BSPTreeNode* rootDest
 );
 
 unsigned int PVS2D_FindLeafOfPoint(
-    PVS2D_BSPTreeNode* root,
-    float x, float y
+	PVS2D_BSPTreeNode* root,
+	float x, float y
+);
+
+int PVS2D_BuildPortals(
+	PVS2D_BSPTreeNode* root
 );
 
 #endif
--- src/pvs2d.c
+++ src/pvs2d.c
@@ -53,13 +53,13 @@ building PVS:
 #ifdef _DEBUG
 #include <stdio.h>
 #include <math.h>
-#define DBG_ASSERT(x, ...) { if (!(x)) { fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); return -1; }}
+#define DBG_ASSERT(x, ret, ...) { if (!(x)) { fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); return ret; }}
 #else
 #define DBG_ASSERT(x, ...)
 #endif
 #endif
 
-#define MATCH_TOLERANCE (0.0625f)
+const float MATCH_TOLERANCE = 0.0625f;
 // the so called EPS. used to fix some errors that inevitably happen with float arithmetics
 
 static inline char _collinear(int ax, int ay, int bx, int by, int cx, int cy) {
@@ -80,7 +80,7 @@ static inline unsigned int _side(float ax, float ay, float bx, float by) {
 
 int _cropSplitSegs(PVS2D_BSPTreeNode* node, PVS2D_Line* line, int left) {
 	if (node->line == line) {
-		DBG_ASSERT(0, "This should not have happened...");
+		DBG_ASSERT(0, -1, "This should not have happened...");
 	}
 	int numer, denom;
 	_intersect(
@@ -207,8 +207,8 @@ char _split(PVS2D_Line* line, PVS2D_Seg* seg, float* tDest) {
 };
 
 int _buildBSP(PVS2D_BSPTreeNode* cur_node, PVS2D_SegStack* cur_segs) {
-	DBG_ASSERT(cur_node, "cur_node can't be NULL (node must be allocated before calling the function)")
-	DBG_ASSERT(cur_segs, "cur_segs can't be NULL (segment array can't have 0 segments)");
+	DBG_ASSERT(cur_node, -1, "cur_node can't be NULL (node must be allocated before calling the function)")
+	DBG_ASSERT(cur_segs, -1, "cur_segs can't be NULL (segment array can't have 0 segments)");
 	cur_node->left = 0;
 	cur_node->right = 0;
 	cur_node->leftLeaf = 0;
@@ -281,18 +281,25 @@ int _buildBSP(PVS2D_BSPTreeNode* cur_node, PVS2D_SegStack* cur_segs) {
 		case SIDE_S_FL:;
 		case SIDE_S_FR:;
 			PVS2D_SegStack* newElem = (PVS2D_SegStack*)malloc(sizeof(PVS2D_SegStack));
-			DBG_ASSERT(newElem, "Failed to allocate new seg stack node");
+			DBG_ASSERT(newElem, -1, "Failed to allocate new seg stack node");
 			*newElem = *curHead;
 			newElem->seg = (PVS2D_Seg*)malloc(sizeof(PVS2D_Seg));
-			DBG_ASSERT(newElem->seg, "Failed to allocate new segment");
+			DBG_ASSERT(newElem->seg, -1, "Failed to allocate new segment");
 			*newElem->seg = *curHead->seg;
 			newElem->seg->tStart = t;
 			curHead->seg->tEnd = t;
-
-			newElem->next = (side == SIDE_S_FL) ? segsLeft : segsRight;
-			curHead->next = (side == SIDE_S_FL) ? segsRight : segsLeft;
-			segsLeft  = (side == SIDE_S_FL) ? newElem : curHead;
-			segsRight = (side == SIDE_S_FL) ? curHead : newElem;
+			if (side == SIDE_S_FL) {
+				newElem->next = segsLeft;
+				segsLeft = newElem;
+				curHead->next = segsRight;
+				segsRight = curHead;
+			}
+			else {
+				newElem->next = segsRight;
+				segsRight = newElem;
+				curHead->next = segsLeft;
+				segsLeft = curHead;
+			}
 			break;
 		default:
 			// something is wrong here
@@ -311,7 +318,7 @@ int _buildBSP(PVS2D_BSPTreeNode* cur_node, PVS2D_SegStack* cur_segs) {
 	}
 	else {
 		PVS2D_BSPTreeNode* newNode = (PVS2D_BSPTreeNode*)malloc(sizeof(PVS2D_BSPTreeNode));
-		DBG_ASSERT(newNode, "Failed to allocate new BSP tree node");
+		DBG_ASSERT(newNode, -1, "Failed to allocate new BSP tree node");
 		int rez = _buildBSP(newNode, segsLeft);
 		if (rez != 0) return rez;   // error encountered
 		cur_node->left = newNode;
@@ -327,7 +334,7 @@ int _buildBSP(PVS2D_BSPTreeNode* cur_node, PVS2D_SegStack* cur_segs) {
 	}
 	else {
 		PVS2D_BSPTreeNode* newNode = (PVS2D_BSPTreeNode*)malloc(sizeof(PVS2D_BSPTreeNode));
-		DBG_ASSERT(newNode, "Failed to allocate new BSP tree node");
+		DBG_ASSERT(newNode, -1, "Failed to allocate new BSP tree node");
 		int rez = _buildBSP(newNode, segsRight);
 		if (rez != 0) return rez;   // error encountered
 		cur_node->right = newNode;
@@ -365,21 +372,21 @@ int PVS2D_BuildBSPTree(int* segs, unsigned int segsC, PVS2D_BSPTreeNode* rootDes
 			}
 		}
 		PVS2D_SegStack* newSeg = (PVS2D_SegStack*)malloc(sizeof(PVS2D_SegStack));
-		DBG_ASSERT(newSeg, "Failed to allocate new segment stack node");
+		DBG_ASSERT(newSeg, -1, "Failed to allocate new segment stack node");
 		newSeg->seg = (PVS2D_Seg*)malloc(sizeof(PVS2D_Seg));
-		DBG_ASSERT(newSeg->seg, "Failed to allocate new segment");
+		DBG_ASSERT(newSeg->seg, -1, "Failed to allocate new segment");
 		float tStart = 0.0f, tEnd = 1.0f;
 		if (match == 0) {
 			_lstack* newLine = (_lstack*)malloc(sizeof(_lstack));
-			DBG_ASSERT(newLine, "Failed to allocate new line into stack");
+			DBG_ASSERT(newLine, -1, "Failed to allocate new line into stack");
 			newLine->line = (PVS2D_Line*)malloc(sizeof(PVS2D_Line));
-			DBG_ASSERT(newLine->line, "Failed to allocate new line in stack node");
+			DBG_ASSERT(newLine->line, -1, "Failed to allocate new line in stack node");
 			newLine->line->ax = ax;
 			newLine->line->ay = ay;
 			newLine->line->bx = bx;
 			newLine->line->by = by;
 			newLine->line->mems = (PVS2D_SegStack*)malloc(sizeof(PVS2D_SegStack));
-			DBG_ASSERT(newLine->line->mems, "Failed to allocate new segment into lines's stack");
+			DBG_ASSERT(newLine->line->mems, -1, "Failed to allocate new segment into lines's stack");
 			newLine->line->mems->seg = newSeg->seg;
 			newLine->line->mems->next = 0;
 			newLine->next = prLines;
@@ -403,7 +410,7 @@ int PVS2D_BuildBSPTree(int* segs, unsigned int segsC, PVS2D_BSPTreeNode* rootDes
 			}
 			newSeg->seg->line = match->line;
 			PVS2D_SegStack* newEntry = (PVS2D_SegStack*)malloc(sizeof(PVS2D_SegStack));
-			DBG_ASSERT(newEntry, "Failed to allocate new entry to global seg stack");
+			DBG_ASSERT(newEntry, -1, "Failed to allocate new entry to global seg stack");
 			newEntry->seg = newSeg->seg;
 			newEntry->next = match->line->mems;
 			match->line->mems = newEntry;
@@ -439,3 +446,453 @@ unsigned int PVS2D_FindLeafOfPoint(PVS2D_BSPTreeNode* root, float x, float y) {
 		return (root->right) ? PVS2D_FindLeafOfPoint(root->right, x, y) : root->rightLeaf;
 	}
 }
+
+typedef struct _pairfc {
+	float p;
+	signed char d;
+} _pairfc;
+
+int _pairfc_cmp(const void* a, const void* b) {
+	float ap = ((_pairfc*)a)->p;
+	float bp = ((_pairfc*)b)->p;
+	if (fabsf(ap - bp) < MATCH_TOLERANCE) {
+		signed char ad = ((_pairfc*)a)->d;
+		signed char bd = ((_pairfc*)b)->d;
+		if (ad < bd) return 1;
+		if (ad > bd) return -1;
+		return 0;
+	}
+	else {
+		if (ap < bp) return -1;
+		if (ap > bp) return 1;
+		return 0;
+	}
+}
+
+// converts node's segments into portals that node contains.
+// it will return the pointer to the stack of created portals
+// or 0 if errors happened
+PVS2D_PortalStack* _portalsOfNode(PVS2D_BSPTreeNode* node) {
+	int segsC = 0;
+	for (PVS2D_SegStack* curSeg = node->segs; curSeg != 0; curSeg = curSeg->next) {
+		if (curSeg->seg->opq)
+			segsC++;
+	}
+	_pairfc* th = calloc(segsC * 2 + 2, sizeof(_pairfc));
+	DBG_ASSERT(th, 0, "Failed to initialize scanline array");
+	segsC = 0;
+	for (PVS2D_SegStack* curSeg = node->segs; curSeg != 0; curSeg = curSeg->next) {
+		if (curSeg->seg->opq) {
+			th[segsC].p = curSeg->seg->tStart;
+			th[segsC++].d = 1;
+			th[segsC].p = curSeg->seg->tEnd;
+			th[segsC++].d = -1;
+		}
+	}
+	th[segsC].p = node->tSplitStart;
+	th[segsC++].d = -1;
+	th[segsC].p = node->tSplitEnd;
+	th[segsC++].d = 1;
+	qsort(th, segsC, sizeof(_pairfc), _pairfc_cmp);
+	int l = 1;
+	float prevSeg = NAN;
+	PVS2D_PortalStack* portals = 0;
+	for (int i = 0; i < segsC; i++) {
+		if (i == 0 && th[i].d == 1) {
+			// we are starting with opaque segment
+			prevSeg = th[i].p;
+		}
+		if (i == segsC - 1 && th[i].d == -1) {
+			// we are ending with opaque segment
+			if (isnan(prevSeg)) {
+				// "outside" segment. happens when there is transparent portal at tSplitStart
+				prevSeg = th[0].p;
+			}
+			PVS2D_PortalStack* newElem = (PVS2D_PortalStack*)malloc(sizeof(PVS2D_PortalStack));
+			DBG_ASSERT(newElem, 0, "Failed to create new portal stack element");
+			newElem->portal = (PVS2D_Portal*)malloc(sizeof(PVS2D_Portal));
+			DBG_ASSERT(newElem->portal, 0, "Failed to create new portal");
+			newElem->portal->seg.line = node->line;
+			newElem->portal->seg.opq = 1;
+			newElem->portal->seg.tStart = prevSeg;
+			newElem->portal->seg.tEnd = th[i].p;
+			newElem->next = portals;
+			portals = newElem;
+			prevSeg = th[i].p;
+			break;
+		}
+		if (th[i].d == 1) {
+			if (l == 0) {
+				// stop previous transparent portal and put it into stack
+				PVS2D_PortalStack* newElem = (PVS2D_PortalStack*)malloc(sizeof(PVS2D_PortalStack));
+				DBG_ASSERT(newElem, 0, "Failed to create new portal stack element");
+				newElem->portal = (PVS2D_Portal*)malloc(sizeof(PVS2D_Portal));
+				DBG_ASSERT(newElem->portal, 0, "Failed to create new portal");
+				newElem->portal->seg.line = node->line;
+				newElem->portal->seg.opq = 0;
+				newElem->portal->seg.tStart = prevSeg;
+				newElem->portal->seg.tEnd = th[i].p;
+				newElem->next = portals;
+				portals = newElem;
+				prevSeg = th[i].p;
+			}
+			l++;
+		}
+		if (th[i].d == -1) {
+			if (l == 1) {
+				// stop previous opaque portal and put it into stack, if it was not portal from "outside"
+				if (isnan(prevSeg)) {
+					// "outside" segment. happens when there is transparent portal at tSplitStart
+					prevSeg = th[i].p;
+					l--;
+					continue;
+				}
+				PVS2D_PortalStack* newElem = (PVS2D_PortalStack*)malloc(sizeof(PVS2D_PortalStack));
+				DBG_ASSERT(newElem, 0, "Failed to create new portal stack element");
+				newElem->portal = (PVS2D_Portal*)malloc(sizeof(PVS2D_Portal));
+				DBG_ASSERT(newElem->portal, 0, "Failed to create new portal");
+				newElem->portal->seg.line = node->line;
+				newElem->portal->seg.opq = 1;
+				newElem->portal->seg.tStart = prevSeg;
+				newElem->portal->seg.tEnd = th[i].p;
+				newElem->next = portals;
+				portals = newElem;
+				prevSeg = th[i].p;
+			}
+			l--;
+		}
+		// we could put here some checks like correct brackets sequence but nah
+	}
+	// i hope there is no need to check for closing the sequence.
+	free(th);
+	return portals;
+}
+
+// the function should not modify the order of elements in adjacents, 
+// but can insert elements
+int _buildPortals(PVS2D_BSPTreeNode* node, PVS2D_PortalStack* adjacent) {
+	// adjacent, if provided, must be circular linked list, meaning that it's "last" element
+	// should just point to the "first", as well as, since it forms an array of 
+	// segment that enclose a certain area, they must be present in counter-clockwise order.
+
+	// first step - split adjacents into left and right ones. since this thing is
+	// present in continous order, there are two nodes, from which start left ones and right ones
+	PVS2D_PortalStack* firstL = 0;
+	PVS2D_PortalStack* firstR = 0;
+	
+	PVS2D_PortalStack* adjCur = 0;
+	PVS2D_PortalStack* adjNxt = adjacent;
+	PVS2D_PortalStack* adjNew = 0;
+	int prside = -1;
+	int loop = 0;
+	while (1) {
+		if (!adjNxt) break;		// adjacents is empty (0)
+		adjCur = adjNxt;
+		adjNxt = adjNxt->next;
+
+		// process the portal
+		float t;
+		char side = _split(node->line, &adjCur->portal->seg, &t);
+		switch (side) {
+		case SIDE_COL:
+			DBG_ASSERT(0, -1, "Incorrect adjacents info");
+			// this should not be possible, since the subspace of a node is a convex shape
+			break;
+		case SIDE_L_PARAL:
+		case SIDE_L_FL:
+		case SIDE_L_FR:
+			// found left
+			if (prside == 0) {
+				// was right
+				firstL = adjCur;
+			}
+			prside = 1;
+			break;
+		case SIDE_R_PARAL:
+		case SIDE_R_FL:
+		case SIDE_R_FR:
+			// found right
+			if (prside == 1) {
+				// was left
+				firstR = adjCur;
+			}
+			prside = 0;
+			break;
+		case SIDE_S_FL:
+		case SIDE_S_FR:
+			// found split
+			adjNew = (PVS2D_PortalStack*)malloc(sizeof(PVS2D_PortalStack));
+			DBG_ASSERT(adjNew, -1, "Failed to create new portal stack node");
+			*adjNew = *adjCur;
+			adjNew->portal = (PVS2D_Portal*)malloc(sizeof(PVS2D_Portal));
+			DBG_ASSERT(adjNew->portal, -1, "Failed to create new portal");
+			*adjNew->portal = *adjCur->portal;
+			// depending on the side the portal we at, put new one at the "end" of the segment
+			// or not, since we can only put new one after ourselves
+			if (adjCur->left) {
+				adjNew->portal->seg.tStart = t;
+				adjCur->portal->seg.tEnd = t;
+			}
+			else {
+				adjNew->portal->seg.tEnd = t;
+				adjCur->portal->seg.tStart = t;
+			}
+			adjNew->next = adjCur->next;
+			adjCur->next = adjNew;
+
+			if (prside != -1) {
+				if (prside == 1)	// was left
+					firstR = adjNew;
+				else				// was right
+					firstL = adjNew;
+				prside = 1 - prside;
+			}
+		}
+
+		// loop two times just for sure
+		if (adjNxt == adjacent) {
+			if (loop == 1) break;
+			loop++;
+		}
+	}
+	// edge_case:
+	//		0 for normal
+	//		1 for no adjacents at all
+	//		2 for no left adjacents
+	//		3 for no right adjacents
+	int edge_case = 0;
+	if (!firstL && !firstR) {
+		edge_case = prside + 2;
+		switch (edge_case)
+		{
+		case 2:
+			firstR = adjacent;
+			break;
+		case 3:
+			firstL = adjacent;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		DBG_ASSERT(firstL && firstR, -1, "Failed to split left and right");
+	}
+
+
+	// step 2 - now that we splitted adjacents (sorta), we need to add portals from our node
+	PVS2D_PortalStack* portals = _portalsOfNode(node);
+	DBG_ASSERT(portals, -1, "Failed to create node's portals");
+	// the portals have the reverse order of node->line, so for the left subspace they will be counterclockwise
+
+	// step 3 - form adjacents for left and right subspace and recurse
+
+	// create adjacents of right subspace 
+	PVS2D_PortalStack* plast = 0;
+	for (PVS2D_PortalStack* prt = portals; ; prt = prt->next) {
+		prt->left = 0;
+		if (!prt->next) {
+			plast = prt;
+			break;
+		}
+	}
+	switch (edge_case) {
+	case 0: 
+		// everything is normal
+		plast->next = firstR;
+		for (PVS2D_PortalStack* prt = firstR; ; prt = prt->next) {
+			if (prt->next == firstL) {
+				prt->next = portals;
+				break;
+			}
+		}
+		break;
+	
+	case 1:
+	case 3: 
+		// no right adjacents (or no adjacents at all)
+		// in each case we don't care about anything other than node's portals
+		plast->next = portals;
+		break;
+	
+	case 2: 
+		// no left adjacents
+		plast->next = firstR;
+		for (PVS2D_PortalStack* prt = firstR; ; prt = prt->next) {
+			if (prt->next == firstR) {
+				prt->next = portals;
+				break;
+			}
+		}
+		break;
+	}
+	// now just feed 'portals' to the recursion
+	if (node->right) {
+		_buildPortals(node->right, portals);
+		// we should get kinda the same thing
+	}
+	else {
+		// right is a leaf, thats good
+		for (PVS2D_PortalStack* prt = portals; ; prt = prt->next) {
+			if (prt->left) {
+				prt->portal->leftLeaf = node->rightLeaf;
+			}
+			else {
+				prt->portal->rightLeaf = node->rightLeaf;
+			}
+			if (prt->next == portals)
+				break;
+		}
+	}
+
+	// now we need to cleanup 'portals' in order to pass it to the left child
+	// the thing is that we will have to reverse the whole stack.
+	// luckily nodes above don't rely on portals of ours, so we can change them as we like
+	PVS2D_PortalStack* tportals = 0;
+	adjCur = 0;
+	adjNxt = portals;
+	while (1) {
+		adjCur = adjNxt;
+		adjNxt = adjNxt->next;
+		if (adjCur->portal->seg.line != node->line) {
+			// since the order of portals is preserved throughout calls
+			// of _buildPortals, our own portals will always be at the beginning
+			// of the stack. so when we find one that isn't we can just exit the loop
+			break;
+		}
+		adjCur->next = tportals;
+		tportals = adjCur;
+		if (adjNxt == portals) {
+			// we looped
+			break;
+		}
+	}
+
+	// next step of cleanup is to connect right to the left
+	// in edge cases 1 and 3 we don't have right adjacents at all
+	// in edge case 2 we just need to connect right to themselves
+	switch (edge_case) {
+	case 2:
+		for (PVS2D_PortalStack* prt = firstR; ; prt = prt->next) {
+			if (prt->next == portals) {
+				prt->next = firstR;
+				break;
+			}
+		}
+		break;
+	case 0:
+		for (PVS2D_PortalStack* prt = firstR; ; prt = prt->next) {
+			if (prt->next == portals) {
+				prt->next = firstL;
+				break;
+			}
+		}
+		break;
+	}
+
+	// now go to left
+	plast = 0;
+	for (PVS2D_PortalStack* prt = tportals; ; prt = prt->next) {
+		prt->left = 1;
+		if (!prt->next) {
+			plast = prt;
+			break;
+		}
+	}
+
+	switch (edge_case) {
+	case 0:
+		// everything is normal
+		plast->next = firstL;
+		for (PVS2D_PortalStack* prt = firstL; ; prt = prt->next) {
+			if (prt->next == firstR) {
+				prt->next = tportals;
+				break;
+			}
+		}
+		break;
+	case 1:
+	case 2:
+		// no left portals (or no portals at all)
+		plast->next = tportals;
+		break;
+	case 3:
+		// no right portals
+		plast->next = firstL;
+		for (PVS2D_PortalStack* prt = firstL; ; prt = prt->next) {
+			if (prt->next == firstL) {
+				prt->next = tportals;
+				break;
+			}
+		}
+		break;
+	}
+	// now we have tportals ready to be fed into left child
+
+	if (node->left) {
+		_buildPortals(node->left, tportals);
+	}
+	else {
+		for (PVS2D_PortalStack* prt = tportals; ; prt = prt->next) {
+			if (prt->left) {
+				prt->portal->leftLeaf = node->leftLeaf;
+			}
+			else {
+				prt->portal->rightLeaf = node->leftLeaf;
+			}
+			if (prt->next == tportals)
+				break;
+		}
+	}
+
+	// step 4 - now have to cleanup:
+
+	// put portals into node
+	adjCur = 0;
+	adjNxt = tportals;
+	while (1) {
+		adjCur = adjNxt;
+		adjNxt = adjNxt->next;
+		if (adjCur->portal->seg.line != node->line) {
+			// since the order of portals is preserved throughout calls
+			// of _buildPortals, our own portals will always be at the beginning
+			// of the stack. so when we find one that isn't we can just exit the loop
+			break;
+		}
+		adjCur->next = node->portals;
+		node->portals = adjCur;
+		if (adjNxt == tportals) {
+			// we looped
+			break;
+		}
+	}
+
+	// next step of cleanup is to connect left to the right.
+	// in edge cases 1 and 2 we don't have left adjacents at all
+	// in edge case 3 we just need to connect left to themselves
+	switch (edge_case) {
+	case 3:
+		for (PVS2D_PortalStack* prt = firstL; ; prt = prt->next) {
+			if (prt->next == tportals) {
+				prt->next = firstL;
+				break;
+			}
+		}
+		break;
+	case 0:
+		for (PVS2D_PortalStack* prt = firstL; ; prt = prt->next) {
+			if (prt->next == tportals) {
+				prt->next = firstR;
+				break;
+			}
+		}
+		break;
+	}
+	// do we need any more cleanup?
+	return 0;
+}
+
+int PVS2D_BuildPortals(PVS2D_BSPTreeNode* root) {
+	return _buildPortals(root, 0);
+}
+
