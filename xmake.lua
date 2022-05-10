a563e652fd32725dd03956b1aa6d11a7957337d2
Laudanum <86309764+Laudanum-dev@users.noreply.github.com>
2022-05-10 18:58:02 +0500

    literally found out how to put headers into package

--- xmake.lua
+++ xmake.lua
@@ -3,8 +3,8 @@ add_rules("mode.debug", "mode.release")
 target("pvs2d")
     set_kind("static")
     add_files("src/pvs2d.c")
-    add_includedirs("include")
     add_includedirs("include", {public = true})
+    add_headerfiles("include/pvs2d.h")
     if is_mode("debug") then
         add_defines("DEBUG")
     end
